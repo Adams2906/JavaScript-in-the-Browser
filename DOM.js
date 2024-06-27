f533441d3874a99171edc266849c952a0b784906
Joshua Solomon Adams <joshuaadams2906@gmail.com>
2024-06-27 18:52:03 +0100

    New commit

---
+++ DOM.js
@@ -0,0 +1,5 @@
+/*
+DOM 
+A browser builds a DOM, or model, for every webpage 
+it downloads as a JavaScript object.
+*/
---
+++ greetings.js
@@ -0,0 +1,13 @@
+ export const informalGreeting = (person) => {
+    console.log(`Hello. ${person}`);
+ }
+
+ export const formalGreeting = (person) => {
+    console.log(`Good day. ${person}`);
+ }
+
+ const greetings = ()=> {
+    console.log(`Howdy!`);
+ }
+
+ export default greetings;
