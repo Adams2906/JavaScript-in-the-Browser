328929321f351cfb48d765172c1352b00c5a6e81
Joshua Solomon Adams <joshuaadams2906@gmail.com>
2024-06-27 14:48:48 +0100

    first commit

---
+++ Modules.js
@@ -0,0 +1,5 @@
+/*JavaScript modules are standalone units of code that you can reuse again and again.
+
+Being standalone means that you can add them to your program, remove them and replace them with other modules and everything will still work
+*/
+
